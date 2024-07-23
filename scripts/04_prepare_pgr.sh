ce15b1b468aac034ddc86cd43100202347aa480a
THION ROMUALD <romuald.thion@univ-lyon1.fr>
2024-07-23 18:04:06 +0200

    bug: ajout timing

--- scripts/04_prepare_pgr.sh
+++ scripts/04_prepare_pgr.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 source ./environment
 
-psql -d $PGDATABASE -f ../database/create_view_pgr.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/create_view_pgr.sql
 psql -d $PGDATABASE -f ../database/alter_table_component.sql
 psql -d $PGDATABASE -f ../database/alter_table_noeud_dittt_ref.sql
 psql -d $PGDATABASE -f ../database/create_table_desserte.sql
--- scripts/05_compute.sh
+++ scripts/05_compute.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 
 source ./environment
 
-psql -d $PGDATABASE -f ../database/insert_table_desserte_dass_etabs_sante.sql
-psql -d $PGDATABASE -f ../database/insert_table_desserte_dimenc_centres.sql
-psql -d $PGDATABASE -f ../database/insert_table_desserte_dimenc_usines.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_dass_etabs_sante.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_dimenc_centres.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_dimenc_usines.sql
 
-psql -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dass_etabs_sante.sql
-psql -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dimenc_centres.sql
-psql -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dimenc_usines.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dass_etabs_sante.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dimenc_centres.sql
+psql -c "\timing on" -d $PGDATABASE -f ../database/insert_table_desserte_aggregate_dimenc_usines.sql
